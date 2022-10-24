16966458e316ca02201a1255a94db47a0c1e513c
xuanvu307 <xuanvufb2@gmail.com>
2022-10-24 23:04:20 +0700

    a

---
+++ src/ex3/View.java
@@ -0,0 +1,16 @@
+package ex3;
+
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class View {
+    public void homeView(){
+        System.out.println("===== WORKER MANAGER =====");
+        System.out.println(" 1. Add worker");
+        System.out.println(" 2. Up salary");
+        System.out.println(" 3. Down salary");
+        System.out.println(" 4. Display information salary");
+        System.out.println(" 5. Exit");
+    }
+
+}
---
+++ src/ex3/WorkerModel.java
@@ -0,0 +1,57 @@
+package ex3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WorkerModel {
+    public Worker inputWorker(Scanner sc){
+        Worker worker = new Worker();
+        System.out.println("enter id: ");
+        worker.setIdWorker(sc.nextLine());
+        System.out.println("enter name: ");
+        worker.setName(sc.nextLine());
+        System.out.println("enter age: ");
+        worker.setAge(sc.nextInt());
+        System.out.println("enter salary: ");
+        worker.setSalary(sc.nextDouble());
+        System.out.println("enter work location: ");
+        worker.setWorkPlace(sc.nextLine());
+        return worker;
+    }
+
+    public void upSalary(ArrayList<Worker> workers, Scanner sc){
+        System.out.println("enter id worker: ");
+        int check = 0;
+        String checkId = sc.nextLine();
+        for (Worker worker: workers){
+            if (checkId.equals(worker.getIdWorker())){
+                System.out.println("enter number plus salary: ");
+                double plusSalary = Double.parseDouble(sc.nextLine());
+                double newSalary = worker.getSalary()+plusSalary;
+                worker.setSalary(newSalary);
+                check ++;
+            }
+        }
+        if (check == 0){
+            System.out.println("wrong id");
+        }
+    }
+
+    public void downSalary(ArrayList<Worker> workers, Scanner sc){
+        System.out.println("enter id worker: ");
+        int check = 0;
+        String checkId = sc.nextLine();
+        for (Worker worker: workers){
+            if (checkId.equals(worker.getIdWorker())){
+                System.out.println("enter number plus salary: ");
+                double plusSalary = Double.parseDouble(sc.nextLine());
+                double newSalary = worker.getSalary()-plusSalary;
+                worker.setSalary(newSalary);
+                check++;
+            }
+        }
+        if (check == 0){
+            System.out.println("wrong id");
+        }
+    }
+}
